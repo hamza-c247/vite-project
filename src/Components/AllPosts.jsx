6598b9e181f08a13a6dc194a4fb9dbdd370ab825
hamzakhan086 <hamzakhancs086@gmail.com>
2022-11-16 00:52:58 +0530

    infinite scroll

--- src/Components/AllPosts.jsx
+++ src/Components/AllPosts.jsx
@@ -1,22 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
 import AllpostUsers from "./AllpostUsers";
 import { useSelector } from "react-redux";
 import SideMenu from "./Common/SideMenu";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 const AllPosts = () => {
   const Users = useSelector((state) => state.User.Users);
-  console.log(Users);
+  const [data, setData] = useState(Users);
+  const [page, setPage] = useState(0),
+    maxPage = Math.ceil(data.length / 5);
 
-  const NoBlogs = ()=>{
-    return(
+  console.log("maxpage", maxPage);
+
+  const onPrevPage = () => {
+    setPage((page + 5 - 1) % maxPage);
+  };
+
+  const fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      setPage((page + 1) % maxPage);
+    }, 1500);
+  };
+  console.log("page", (page + 1) % maxPage);
+
+  console.log("sliceeee", data.slice(page * 5, 5 * (page + 1)));
+
+  const NoBlogs = () => {
+    return (
       <>
-      <div className="no-blogs">
-        No blog available
-      </div>
+        <div className="no-blogs">No blog available</div>
       </>
-    )
-  }
+    );
+  };
 
   return (
     <>
@@ -24,14 +42,25 @@ const AllPosts = () => {
         <SideMenu></SideMenu>
       </div>
       <div className="Blog-heading">
-        <h2> <i>Recent Blogs</i></h2>
+        <h2>
+          {" "}
+          <i>Recent Blogs</i>
+        </h2>
       </div>
       <div className="blog-data">
-        {Users.map((user) => (
-          <AllpostUsers key={user.id} user={user} />
-        ))}
+        <InfiniteScroll
+          dataLength={Users.length} //This is important field to render the next data
+          next={fetchMoreData}
+          hasMore={true}
+          onScroll={onPrevPage}
+          loader={<h4>Loading...</h4>}
+        >
+          {data.slice(page * 5, 5 * (page + 1)).map((user) => (
+            <AllpostUsers key={user.id} user={user} />
+          ))}
+        </InfiniteScroll>
       </div>
-      {Users.length === 0 && <NoBlogs/>}
+      {Users.length === 0 && <NoBlogs />}
     </>
   );
 };
